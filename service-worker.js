c17cc8e9e56426f68b81d661b8bcef1cf0bd7085
pgalan94 <69272435+pgalan94@users.noreply.github.com>
2023-08-16 17:38:38 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,42 +2,42 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-var urlsToCache = [ 'index.html',
-                    'favicon.ico',  
-                    'manifest.json' ];
+var urlsToCache = ['index.html',
+  'favicon.ico',
+  'manifest.json'];
 
-self.addEventListener( 'install', function(){
-  return self.skipWaiting( );
+self.addEventListener('install', function () {
+  return self.skipWaiting();
 });
 
-self.addEventListener( 'activate', function( event ) {
+self.addEventListener('activate', function (event) {
   event.waitUntil(
     caches.open('v1')
-      .then( function( cache ) {
-        return cache.addAll( urlsToCache );
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
       })
-      .then( function() {
-        return self.clients.claim() ;
-      }) 
-      .catch( function(e) {
+      .then(function () {
+        return self.clients.claim();
+      })
+      .catch(function (e) {
         console.log("Error handling cache", e);
       })
   );
 });
 
-self.addEventListener( 'fetch', (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
         return response;
       }
-      return fetch(event.request);
+      return fetch(event.request).then((resp) => {
+        return caches.open('v1').then((cache) => {
+          cache.put(event.request, resp.clone());
+          return resp;
+        });
+      });
     })
-  ).then( (resp) => {
-    return caches.open('v1').then( (cache) => {
-      cache.put( event.request, resp.clone( ));
-        return resp;
-    });
-  });
+  );
 });
 
